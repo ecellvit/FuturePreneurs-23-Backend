5f61fac06eddc3f4f6b044883225c963133ebea3
Aryan Kumar <86525435+AryanKum99@users.noreply.github.com>
2023-09-27 00:00:04 +0530

    Initial Auth and sample Team Route

---
+++ models/teamModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const teamSchema = mongoose.Schema(
+    {
+        teamName: {
+            type: String,
+            unique: true
+        },
+        teamLeaderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Users",
+        },
+        teamNumber: {
+            type: Number
+        },
+        leaderName: {
+            type: String
+        },
+        leaderEmail: {
+            type: String
+        },
+        teamID: {
+            type: mongoose.Schema.Types.ObjectId
+        },
+        isQualified: {
+            type: Boolean
+        },
+        currentRound: {
+            type: String
+        }
+    },
+    { collection: "TeamModel" }
+);
+
+module.exports = mongoose.model("TeamModel", teamSchema);
---
+++ routes/team.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const team = require('../controllers/team/team');
+const auth = require('../middleware/authmiddleware');
+router.route('/getTeam')
+    .get(auth, team.getTeam)
+router.route('/createTeam')
+    .post(auth, team.makeTeam)
+
+module.exports = router;
