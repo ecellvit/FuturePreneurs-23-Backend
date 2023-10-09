82423da12a791d366c09f0ce24debb0f2484db0b
smritidoneria <smritidoneria@gmail.com>
2023-10-09 22:38:34 +0530

    added team_routes

--- models/teamModel.js
+++ models/teamModel.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 const teamSchema = mongoose.Schema(
     {
@@ -26,9 +27,55 @@ const teamSchema = mongoose.Schema(
         },
         currentRound: {
             type: String
+        },
+        members:{
+            type:[Number]
         }
     },
     { collection: "TeamModel" }
 );
 
 module.exports = mongoose.model("TeamModel", teamSchema);
+
+/*
+const mongoose = require('mongoose');
+const teamSchema = mongoose.Schema(
+    {
+        teamName: {
+            type: String,
+            unique: true
+        },
+        teamLeaderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Users",
+        },
+        teamNumber: {
+            type: Number
+        },
+        leaderName: {
+            type: String
+        },
+        leaderEmail: {
+            type: String
+        },
+        teamID: {
+            type: mongoose.Schema.Types.ObjectId
+        },
+        isQualified: {
+            type: Boolean
+        },
+        currentRound: {
+            type: String
+        },
+        teamToken:{
+            type:String
+        },
+        members:{
+            type:[Number]
+        }
+    },
+    { collection: "TeamModel" }
+);
+
+module.exports = mongoose.model("TeamModel", teamSchema);
+*/
