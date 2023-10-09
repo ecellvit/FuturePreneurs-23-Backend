259fa81e3ce2fa0356dfaf2493bf3f70f91fc688
smritidoneria <smritidoneria@gmail.com>
2023-10-09 20:57:33 +0530

    added team routes

--- routes/team.js
+++ routes/team.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const router = express.Router();
 const team = require('../controllers/team/team');
@@ -7,4 +8,23 @@ router.route('/getTeam')
 router.route('/createTeam')
     .post(auth, team.makeTeam)
 
+module.exports = router;
+*/
+const express = require('express');
+const router = express.Router();
+const team = require('../controllers/team');
+//const auth = require('../middleware/authmiddleware');
+
+router.route('/getTeam')
+    .get( team.getTeam)
+    
+router.route('/createTeam')
+    .post( team.makeTeam)
+
+router.route('/deleteTeam')
+    .post(team.deleteTeam)  
+router.route('/admin')
+    .get(team.removeMember)     
+router.route('/gettoken')
+    .get(team.getTeamToken)
 module.exports = router;
